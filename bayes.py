50670bc5f0f39d799d1c3114db3164bc52dff676
Subash Prakash <subashprakash8@gmail.com>
2017-12-19 14:24:22 +0100

    Calculating prior probablity

--- bayes.py
+++ bayes.py
@@ -1,7 +1,9 @@
 import random 
 import csv
 import math
+from decimal import Decimal
 data=[]
+
 def loadCsv(filename):
     with open(filename) as csvDataFile:
         csvReader = csv.reader(csvDataFile)
@@ -9,13 +11,32 @@ def loadCsv(filename):
             data.append(row)
     return data
 
+def calcPriorProbablity(priorProbCounts,trainingData):
+    length = len(trainingData[0]) -1
+    for val in trainingData:
+        if not val[length] in priorProbCounts:
+            print("Entered")
+            priorProbCounts[val[length]] = 0
+        else:
+            #values = priorProb[val[6]]
+            priorProbCounts[val[length]] = priorProbCounts.get(val[length]) + 1
+            
+    return priorProbCounts
 
 def splitData(dataset,splitRatio):
-    random.shuffle(dataset)
+    #random.shuffle(dataset)
     #print(len(dataset))
     trainSet=dataset[:(int)(len(dataset)*splitRatio)] 
     testSet=dataset[(int)(len(trainSet)):]
     return[trainSet,testSet]
+    
+def priorProb(classification,length):
+    priorProbablity = {}
+    for i in classification:
+        priorProbablity[i]=Decimal(classification[i]/length)
+        
+        
+    return priorProbablity
 
 def main():
     filename = 'car.csv'
@@ -23,7 +44,15 @@ def main():
     dataset = loadCsv(filename)
     trainingData,testData = splitData(dataset, splitRatio)
     
+    target='class'
+    attributes = ['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'class']
+    
+    print(trainingData[0])
+    priorProbCounts = {}
+    priorProbCounts = calcPriorProbablity(priorProbCounts,trainingData)
+    priorProbablity = priorProb(priorProbCounts,len(trainingData))
     print(len(trainingData))
     print(len(testData))
-
+    print(priorProbCounts)
+    print(priorProbablity)
 main()
