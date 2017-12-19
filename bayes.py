7b47c61bcf6e5e57099d2d8cf911c94bac8cfbae
Subash Prakash <subashprakash8@gmail.com>
2017-12-19 12:28:01 +0100

    Initial changes for the code

--- bayes.py
+++ bayes.py
@@ -3,32 +3,27 @@ import csv
 import math
 data=[]
 def loadCsv(filename):
-	with open('car.csv') as csvDataFile:
-		csvReader = csv.reader(csvDataFile)
-		for row in csvReader:
-			data.append(row)
-	return data
+    with open(filename) as csvDataFile:
+        csvReader = csv.reader(csvDataFile)
+        for row in csvReader:
+            data.append(row)
+    return data
+
+
 def splitData(dataset,splitRatio):
-	random.shuffle(dataset)
-	print(len(dataset))
-	trainSet=dataset[:(int)(len(dataset)*splitRatio)] 
-	testSet=dataset[(int)(len(trainSet)):]
-	return[trainSet,testSet]
-
-
-
-
-
+    random.shuffle(dataset)
+    #print(len(dataset))
+    trainSet=dataset[:(int)(len(dataset)*splitRatio)] 
+    testSet=dataset[(int)(len(trainSet)):]
+    return[trainSet,testSet]
 
 def main():
-	filename = 'car.csv'
-	splitRatio = 0.75
-	dataset = loadCsv(filename)
-	trd,td=splitData(dataset, splitRatio)
-
-	print(td)
-	print(len(trd))
-	print(len(td))
+    filename = 'car.csv'
+    splitRatio = 0.75
+    dataset = loadCsv(filename)
+    trainingData,testData = splitData(dataset, splitRatio)
+    
+    print(len(trainingData))
+    print(len(testData))
 
 main()
- 
